cdd98f241131c13c1425fa7d258a828a2116e313
Mohsen Ansari <mansari@deloitte.ca>
2019-07-22 09:05:02 -0400

    added more unit test

--- src/test/java/com/vitter/rest/util/TextUtilsTest.java
+++ src/test/java/com/vitter/rest/util/TextUtilsTest.java
@@ -19,7 +19,7 @@ public class TextUtilsTest {
 	public void tearDown() throws Exception {
 		tu = null;
 	}
-
+    //count letters unit tests
 	@Test
 	public void countLettersTest() {
         int result = tu.countLetters("test");
@@ -42,5 +42,36 @@ public class TextUtilsTest {
 
         result = tu.countLetters("abc    ");
         assertEquals(3, result);
+    }
+    // count words unittests
+    @Test
+    public void countWordsTest() {
+        int result = tu.countWords("hello this is a sentence");
+        assertEquals(5, result);
+    }
+
+    public void countWordsEmpty() {
+        int result = tu.countWords("");
+        assertEquals(0, result);
+    }
+    @Test
+    public void countWordsWithSpaces() {
+        int result = tu.countWords("   test");
+        assertEquals(1, result);
+
+        result = tu.countWords("test     ");
+        assertEquals(1, result);
+
+        result = tu.countWords("   test1   test2    ");
+        assertEquals(2, result);
+    }
+
+    //most repeated word unittests
+    // example of a failing unit test
+    @Test
+    public void mostRepeatedWordTest() {
+        String result = tu.mostRepeatedWord("Test string is a test."); //expected return: test
+        assertEquals("test", result); // should ignore uppercase and lowercase
+        // result of this test: add uppercase and lowercase support
     }
 }
